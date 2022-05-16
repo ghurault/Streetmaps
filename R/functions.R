54eb5ac55bd141090b2ebb468efd7a92f3d5ee34
Guillem Hurault <guillem.hurault16@imperial.ac.uk>
2022-05-16 18:56:41 +0100

    Refactor documentation

--- R/functions.R
+++ R/functions.R
@@ -9,29 +9,24 @@ library(sf)
 
 # Functions ---------------------------------------------------------------
 
+#' Generate coordinates matrix
+#'
+#' @param x Vector of longitudes of the bottom left corner and the top right corner
+#' @param y Vector of latitudes of the bottom left corner and the top right corner
+#'
+#' @return Matrix
 make_coordinates <- function(x, y) {
-  # Generate coordinates matrix
-  #
-  # Args:
-  # x: vector of longitudes of the bottom left corner and the top right corner
-  # y: vector of latitudes of the bottom left corner and the top right corner
-  #
-  # Returns:
-  # Matrix
-  
   matrix(c(x, y),
          nrow = 2, ncol = 2, byrow = TRUE,
          dimnames = list(c("x", "y"), c("min", "max")))
 }
 
+#' Compute ratio of distance covered by the x-axis by the distance covered by the y-axis
+#'
+#' @param coordinates Matrix of coordinates
+#'
+#' @return Numeric ratio
 ratio_xy <- function(coordinates) {
-  # Compute ratio of distance covered by the x-axis by the distance covered by the y-axis
-  #
-  # Args:
-  # coordinates: Matrix of coordinates
-  #
-  # Returns:
-  # Numeric ratio
   
   library(geosphere)
   
